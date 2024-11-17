abd2f4a3b137fa393d8ac42a30f781975d191ab4
AJ Barea <ajbareaa@gmail.om>
2024-11-17 04:15:35 -0500

    Add .gitignore to exclude coverage directory and update HomeScreen test import example as to not launch welcome screen on emulator

--- OneStopShop/__tests__/HomeScreen-test.tsx
+++ OneStopShop/__tests__/HomeScreen-test.tsx
@@ -1,7 +1,7 @@
 // TODO: Remove this file when more unit tests are set up
+import HomeScreen from '@/components';
 import { render } from '@testing-library/react-native';
 
-import HomeScreen, { CustomText } from '@/app/index';
 
 describe('<HomeScreen />', () => {
   test('Text renders correctly on HomeScreen', () => {
