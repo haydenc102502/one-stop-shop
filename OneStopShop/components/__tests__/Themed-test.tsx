c07902806e8270058648b304f5912d5846bf16b9
AJ Barea <ajbareaa@gmail.om>
2024-11-19 17:49:38 -0500

    Enhance Themed component tests to 100% coverage with additional scenarios for light and dark themes

--- OneStopShop/components/__tests__/Themed-test.tsx
+++ OneStopShop/components/__tests__/Themed-test.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import renderer from 'react-test-renderer';
 import { Text, View } from '../Themed';
+import * as useColorSchemeModule from '../useColorScheme';
+
+jest.mock('../useColorScheme');
 
 describe('Themed components', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
   it('Text component renders correctly', () => {
     const tree = renderer.create(<Text>Snapshot test for Text!</Text>).toJSON();
     expect(tree).toMatchSnapshot();
@@ -10,6 +17,28 @@ describe('Themed components', () => {
 
   it('View component renders correctly', () => {
     const tree = renderer.create(<View><Text>Snapshot test for View!</Text></View>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Text component uses lightColor prop when provided', () => {
+    const tree = renderer.create(<Text lightColor="red">Test with lightColor</Text>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('View component uses darkColor prop when provided', () => {
+    const tree = renderer.create(<View darkColor="blue"><Text>Test with darkColor</Text></View>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Text component uses default theme when useColorScheme returns null', () => {
+    (useColorSchemeModule.useColorScheme as jest.Mock).mockReturnValue(null);
+    const tree = renderer.create(<Text>Test with default theme</Text>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Text component uses light theme when useColorScheme returns light', () => {
+    (useColorSchemeModule.useColorScheme as jest.Mock).mockReturnValue('light');
+    const tree = renderer.create(<Text>Test with light theme</Text>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
